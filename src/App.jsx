00119c84d55dbfdb25ad94c69c15412922ccadd6
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-20 12:16:14 +0330

    install react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return <div>Hello World!!</div>;
+  return (
+    <div>
+      <Header />
+      <Container>
+        <h2>FC Market</h2>
+      </Container>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
