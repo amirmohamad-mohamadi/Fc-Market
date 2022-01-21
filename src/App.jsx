e912218eb76cccf0a9c0b7697b9a2773428eed79
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-22 01:34:08 +0330

    add react-router-dom and create links. build  productList

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
       <main>
         <Container className="py-3">
-          <h2>FC Market</h2>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<Product />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
