34d9f2c496a171b88d3ebfd4d6c5d5f45d0e6036
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-23 02:44:45 +0330

    Bring product with redux and crate cart page

--- src/action/cartAction.js
+++ src/action/cartAction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const cartAction = (id) => async (dispatch, getState) => {
-  const { data } = axios.get(`http://localhost:8000/api/products/${id}`);
+  const { data } = await axios.get(`http://localhost:8000/api/products/${id}`);
 
   dispatch({
     type: "Cart_ADD_ITEMS",
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Row, Col, ListGroup, Image, Button } from "react-bootstrap";
+
+import { cartAction } from "../action/cartAction";
 
 const Cart = () => {
-  return <div>Cart aslfdnsfnsdf</div>;
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  console.log(cartItems);
+  useEffect(() => {
+    dispatch(cartAction(id));
+  }, [dispatch, id]);
+
+  const removeFromCartHandler = (id) => {
+    console.log("REMOVE");
+  };
+
+  return (
+    <div>
+      <Row>
+        <Col md={8}>
+          <h2>سبد خرید</h2>
+          {cartItems.lenght === 0 ? (
+            <h2>سبدخرید خالی است</h2>
+          ) : (
+            <ListGroup variant="flush">
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item.product}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid />
+                    </Col>
+                    <Col md={3}>{item.name}</Col>
+                    <Col md={2}>{item.price}</Col>
+                    <Col md={2}>
+                      <Button
+                        type="button"
+                        onClick={() => removeFromCartHandler(item.product)}
+                        variant="light"
+                      >
+                        <i className="fa fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Cart;
